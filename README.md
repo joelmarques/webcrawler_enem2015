3d75f3f1f5d952eec304ef52a31279a40d832454
Joel Marques <bitmarques@gmail.com>
2015-11-06 17:59:23 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Enem 2015 Webcrawler
+Webcrawler que mostra as questões comentadas do primeiro e segundo dia do enem 2015.
---
+++ enem2015_1dia.js
@@ -0,0 +1,28 @@
+/*
+  Busca todos os links de todas as questões do primeiro dia da prova do enem 2015
+*/
+var request = require('request');
+var cheerio = require('cheerio');
+var urlbase = "http://www.curso-objetivo.br/vestibular/resolucao_comentada/enem/enem2015_1dia.asp?img=01";
+request(urlbase, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // console.log(body);
+
+        $ = cheerio.load(body);
+
+        $('table td a').each(function(index,value){
+          /*itera cada td*/
+          var obj = {};
+
+          obj.questao = $(this).text();
+
+          var linkCompleto = new String($(this).attr('href'));
+
+          var sep1 = linkCompleto.split('("').pop();
+
+          obj.link = sep1.split('",').shift();
+
+          console.log(obj);
+        });
+    }
+})
